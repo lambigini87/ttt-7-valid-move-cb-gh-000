330495d643dadc3f41e5fb13dfa1da2edb3a1df6
harrison tran <harrisontran@outlook.com>
2018-06-22 07:37:47 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-    if board.empty?
+    if board.empty? == true
       return true
 
     elsif position_taken?(board, index) == true
