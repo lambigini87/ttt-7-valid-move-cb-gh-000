b2f0715023ffc8a18be0c89ee254699eaddfa2f4
harrison tran <harrisontran@outlook.com>
2018-06-22 07:29:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,8 +3,8 @@ def valid_move?(board, index)
     if position_taken?(board, index) == true
       return true
 
-    elsif index.between?(1,9)
-      return true
+    elsif index.between?(1,9) == false
+      return false
 
     else
       return false
